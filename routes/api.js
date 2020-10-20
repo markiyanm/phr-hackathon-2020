aa7733fa1f4b5a929c338a6ec33b8974c0717c9b
Markiyan <markiyanm@gmail.com>
2020-10-20 16:23:54 -0400

    testing

--- routes/api.js
+++ routes/api.js
@@ -19,15 +19,17 @@ router.post('/', (req, res) => {
     
     console.log(lead);
 
-    lead.save()
-        .then(data => {
-            console.log('in the then clause');
-            res.json(data);
-        })
-        .catch(err => {
-            console.log('in the error clause');
-            res.json({ message: err });
-        });
+    res.json(lead);
+
+    // lead.save()
+    //     .then(data => {
+    //         console.log('in the then clause');
+    //         res.json(data);
+    //     })
+    //     .catch(err => {
+    //         console.log('in the error clause');
+    //         res.json({ message: err });
+    //     });
 });
 
 module.exports = router;
